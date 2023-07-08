677868b4318b491002e5465c6f688628d8f7f655
Vishal Sharma <svsharma918277@gmail.com>
2023-07-08 18:20:25 +0530

    cw7th

--- ecom app/signin.js
+++ ecom app/signin.js
@@ -1,34 +1,34 @@
 const password = document.getElementById("password");
-const passwordcheckbox=document.getElementById("showPasswordCheckbox");
-passwordcheckbox.addEventListener("change", function() {
-    if (passwordcheckbox.checked) {
-      password.type = "text";
-    } else {
-      password.type = "password";
-    }
-  });
+const passwordcheckbox = document.getElementById("showPasswordCheckbox");
+passwordcheckbox.addEventListener("change", function () {
+  if (passwordcheckbox.checked) {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+});
 
-  document.getElementById("signup").addEventListener("click",gotosignup);
-  function gotosignup(){
-    window.location.href="./signUp.html"
+document.getElementById("signup").addEventListener("click", gotosignup);
+function gotosignup() {
+  window.location.href = "./signUp.html"
+}
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+
+  const users = JSON.parse(localStorage.getItem("userinfo"));
+  let loggedIn = false;
+  if (email === "" || password === "") {
+    alert("Please fill in all fields.");
   }
 
-  const form = document.querySelector("form");
-
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      
-
-      const users = JSON.parse(localStorage.getItem("userinfo"));
-      let loggedIn = false;
-      if (email === "" || password === "") {
-        alert("Please fill in all fields.");
-      }
-
-   else if (Array.isArray(users) && users.length > 0) {
+  else if (Array.isArray(users) && users.length > 0) {
     for (let i = 0; i < users.length; i++) {
       const user = users[i];
       if (user.email === email && user.password === password) {
@@ -37,10 +37,10 @@ passwordcheckbox.addEventListener("change", function() {
       }
     }
     if (loggedIn) {
-      alert("Login successful" );
-      location.href = "../";
+      alert("Login successful");
+      window.location.href = "../index.html";
     } else {
       alert("Invalid email or password");
     }
   }
-    });
+});
